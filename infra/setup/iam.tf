71d15040fa76f4bf10c3f943e521bf886a6a8363
devopscoacht <devopstrainingwitcoacht@gmail.com>
2025-02-14 14:16:20 +0100

    updated readme

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -21,8 +21,6 @@ data "aws_iam_policy_document" "tf_backend" {
     resources = ["arn:aws:s3:::${var.tf_state_bucket}"]
   }
 
-
-
   statement {
     effect  = "Allow"
     actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
@@ -45,7 +43,7 @@ data "aws_iam_policy_document" "tf_backend" {
 }
 
 resource "aws_iam_policy" "tf_backend" {
-  name        = "${aws_iam_user.cd.name}-tf-s3-dynamic"
+  name        = "${aws_iam_user.cd.name}-tf-s3-dynamodb"
   description = "Allow user to use S3 and DynamoDB for TF backend resources"
   policy      = data.aws_iam_policy_document.tf_backend.json
 }
